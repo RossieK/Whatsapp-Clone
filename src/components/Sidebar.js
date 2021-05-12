7af390c76c2e8c64fc5f0fd0a6af145b27dc3232
RossieK <rosi.kirova96@gmail.com>
2021-05-12 15:56:22 +0300

    Implement sidebar chats

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,7 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import {Avatar, IconButton} from "@material-ui/core";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
@@ -28,6 +29,12 @@ function Sidebar() {
           <SearchOutlinedIcon />
           <input placeholder="Search or start new chat" type="text" />
         </div>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
