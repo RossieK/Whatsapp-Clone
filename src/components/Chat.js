12e774b2c92bf76ef1c67982ba35cfb1320c0ec0
RossieK <rosi.kirova96@gmail.com>
2021-05-12 16:17:20 +0300

    Implement chat section header

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,30 @@
 import "../style/Chat.css";
+import {Avatar, IconButton} from "@material-ui/core";
+import AttachFileOutlinedIcon from "@material-ui/icons/AttachFileOutlined";
+import MoreVertOutlinedIcon from "@material-ui/icons/MoreVertOutlined";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>Chat box</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertOutlinedIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
