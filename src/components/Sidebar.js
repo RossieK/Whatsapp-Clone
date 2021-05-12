91752c6da97ae3a368b78a957083e8d97dfc1835
RossieK <rosi.kirova96@gmail.com>
2021-05-12 14:50:40 +0300

    Implement sidebar header
    Add material-ui

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,26 @@
 import "../style/Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {Avatar, IconButton} from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar src="https://creative.unab.edu.co/wp-content/uploads/2019/02/avatar.png" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
