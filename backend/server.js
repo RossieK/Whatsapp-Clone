c10610c17e241a81817692c9f902c7fd505998c4
RossieK <rosi.kirova96@gmail.com>
2021-05-12 18:52:04 +0300

    Add mongoDB connection and message model

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config({path: "./.env"});
 const app = express();
 const port = process.env.PORT || 9000;
 
+//MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", function callback() {
+  console.log("DB connected...");
+});
+
+//Routes
 app.get("/", (req, res) => {
   res.status(200).send("hello");
 });
 
+//Listen
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
