7f75d4fc84f8f23cb4377903452906c9b4c4661e
RossieK <rosi.kirova96@gmail.com>
2021-05-13 14:27:29 +0300

    Add pusher to the FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import "./style/App.css";
+import Pusher from "pusher-js";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import {useEffect} from "react";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("1b880ea365d7733fc7e7", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (data) {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="app__body">
