ece88adf78da1d6b7318dac3f4b1049be3f0ae36
RossieK <rosi.kirova96@gmail.com>
2021-05-13 15:12:22 +0300

    Add messages fetching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,35 @@ import "./style/App.css";
 import Pusher from "pusher-js";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
-import {useEffect} from "react";
+import axios from "./axios";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((res) => {
+      setMessages(res.data);
+    });
+  }, []);
+
   useEffect(() => {
     const pusher = new Pusher("1b880ea365d7733fc7e7", {
       cluster: "eu",
     });
 
     const channel = pusher.subscribe("messages");
-    channel.bind("inserted", function (data) {
-      alert(JSON.stringify(data));
+    channel.bind("inserted", function (newMessage) {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage]);
     });
-  }, []);
+    console.log(messages);
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
 
   return (
     <div className="App">
