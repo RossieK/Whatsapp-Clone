dea4c595264961790a36e0b15590a1cfb1a6c85d
RossieK <rosi.kirova96@gmail.com>
2021-05-24 18:35:20 +0300

    Add register endpoint

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -1,2 +1,37 @@
 const router = require("express").Router();
+const registerValidator = require("../middlewares/userMiddlewareValidator");
+const formValidator = require("../middlewares/formValidator");
+const userService = require("../services/userService");
+
+router.post("/", registerValidator, (req, res) => {
+  const {email, password, rePassword} = req.body;
+
+  if (!email || !password || !rePassword) {
+    return res.status(400).json({message: "All fields are required"});
+  }
+
+  const formValidations = formValidator(req);
+
+  if (!formValidations.isValid) {
+    return res.status(400).json({message: `${formValidations.message}`});
+  }
+
+  userService
+    .register({email, password})
+    .then(async (user) => {
+      let token = await userService.loginUponRegistration(user);
+
+      res.json({
+        token,
+        user: {
+          id: user._id,
+          email: user.email,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({message: `${err}`});
+    });
+});
+
 module.exports = router;
---
+++ backend/services/userService.js
@@ -0,0 +1,29 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+async function register(data) {
+  const {email, password} = {...data};
+
+  let foundUserEmail = await User.findOne({email});
+
+  if (foundUserEmail) {
+    throw "The given email is already in use";
+  }
+
+  let salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS));
+  let hash = await bcrypt.hash(password, salt);
+
+  const user = new User({email, password: hash});
+  return user.save();
+}
+
+function loginUponRegistration(user) {
+  let token = jwt.sign({_id: user._id}, process.env.BCRYPT_SECRET);
+  return token;
+}
+
+module.exports = {
+  register,
+  loginUponRegistration,
+};
